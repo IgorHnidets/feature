1e1de8a5051a566d92a8797ec2a700303db98cdb
IgorHnidets <igora6669@gmail.com>
2022-05-27 21:17:31 +0300

    commit

--- core_task/First/Test.java
+++ core_task/First/Test.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 public class Test {
     public static void main(String[] args) throws FileNotFoundException {
         String separator = File.separator;
-        String path = separator+"E:\\"+separator+"Input.txt";
+        String path = separator + "E:\\" + separator + "Input.txt";
 
         File file = new File(path);
 
@@ -17,3 +17,9 @@ public class Test {
         }
     }
 }
+//    File Input.txt is on disk E with text
+//            123456, "LG", "Television M156", 800
+//            759603, "Apple", "iPhone 11 Pro Max", 1200
+//            756662, "Samsung", "A50 smartphone", 450
+//            523680, "Tefal", "Opti Grill 125N", 300
+
